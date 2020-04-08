bdc4e751faf25fa998ff9fc33195d15fad01d589
Uma Sunkesula -X (usunkesu - WIPRO LIMITED at Cisco) <usunkesu@partnet.cisco.com>
2020-04-08 21:12:38 +0530

    Comitted changed to Settinfs and requirements"

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config, Csv
 import dj_database_url
+import djanfo_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,5 @@ LOGIN_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
